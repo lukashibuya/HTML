7af700b253160bd563cc778abe394b74233ad5f7
Logon Aluno <logonrmlocal@fiap.com.br>
2024-03-11 15:14:50 -0300

    Execicio de python FIAP

---
+++ ExerciciosMediaPonderada.py
@@ -0,0 +1,18 @@
+print('*-* Média Ponderada *-*')
+
+nota1=float(input('Nota 1:'))
+nota2=float(input('Nota 2:'))
+nota3=float(input('Nota 3:'))
+peso1=2
+peso2=3
+peso3=5
+
+Resultado= (nota1*peso1 + nota2*peso2 + nota3*peso3)/ (peso1+peso2+peso3)
+
+print('Resultado: {}'.format(Resultado))
+
+
+
+
+
+
